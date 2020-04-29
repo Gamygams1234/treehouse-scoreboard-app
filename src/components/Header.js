1d1e6901bcf4d7a9fa774eded87b351b71055f6b
Gamygams1234 <gamyburgos@gmail.com>
2020-04-29 16:12:59 -0700

    Building the Statistics Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Stats from "./Stats";
 
 export default function Header(props) {
   return (
     <header>
+      <Stats players={props.totalPlayers} />
       <h1>{props.title}</h1>
-      <span className="stats">Players: {props.totalPlayers}</span>
     </header>
   );
 }
