8af2af8581e481dea9e2d0ee78cee3fc993d2024
Gamygams1234 <gamyburgos@gmail.com>
2020-04-29 17:08:48 -0700

    My solution to the form

---
+++ src/components/AddPlayerForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class AddPlayerForm extends Component {
+  state = {
+    name: "",
+  };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.handleAddPlayer(this.state);
+    this.setState({
+      name: "",
+    });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input onChange={this.handleChange} type="text" id={"name"} value={this.state.name} placeholder="Enter a player's name" />
+        <input type="submit" placeholder="Add Player" />
+      </form>
+    );
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Player from "./Player";
+import AddPlayerForm from "./AddPlayerForm";
 
 class App extends Component {
   state = {
     players: [
       {
-        name: "Guil",
+        name: "Gamy",
         score: 0,
         id: 1,
       },
       {
-        name: "Treasure",
+        name: "Chris",
         score: 0,
         id: 2,
       },
       {
-        name: "Ashley",
+        name: "Ronak",
         score: 0,
         id: 3,
       },
       {
-        name: "James",
+        name: "Lisa",
         score: 0,
         id: 4,
       },
     ],
   };
   // writing the handle score change to get passed to the child
+  handleAddPlayer = (player) => {
+    player.id = Math.random();
+    player.score = 0;
+    let players = [...this.state.players, player];
+    this.setState({
+      players: players,
+    });
+  };
   handleScoreChange = (index, delta) => {
     this.setState((prevState) => ({
       score: (prevState.players[index].score += delta),
@@ -50,6 +59,7 @@ class App extends Component {
         {this.state.players.map((player, index) => (
           <Player name={player.name} id={player.id} key={player.id.toString()} index={index} removePlayer={this.handleRemovePlayer} handleScoreChange={this.handleScoreChange} score={player.score} />
         ))}
+        <AddPlayerForm handleAddPlayer={this.handleAddPlayer} />
       </div>
     );
   }
